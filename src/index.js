40e2a5f48d6737fddd66e1b88c878b06f4a59997
Merhan Elkheshen <45357058+miniEl@users.noreply.github.com>
2020-11-17 04:44:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 import middleware from './middleware';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
