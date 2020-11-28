b6a0ba4ff07497cefcbec37b9c82d8e41fd51bfc
Merhan Elkheshen <merhanelkheshen@gmail.com>
2020-11-28 23:19:51 +0200

    Added:
    - Created 'NewQuestion' component

--- src/components/NewQuestion/NewQuestion.js
+++ src/components/NewQuestion/NewQuestion.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Card, Container } from 'react-bootstrap';
+import { Button, Card, Container, Form } from 'react-bootstrap';
+import './NewQuestion.css';
 
 class NewQuestion extends Component {
   state = {
@@ -12,10 +13,20 @@ class NewQuestion extends Component {
       <Container className="new-que-container">
         <Card expand="sm" className="new-que-card">
           <Card.Header>
-
+            <Card.Title className="primary-text">Create New Question</Card.Title>
           </Card.Header>
           <Card.Body>
-
+            <Card.Title>Would you rather ...</Card.Title>
+            <Form>
+              <Form.Group>
+                <Form.Control type="text" placeholder="Enter Option One"></Form.Control>
+              </Form.Group>
+              <Card.Text>OR</Card.Text>
+              <Form.Group>
+                <Form.Control type="text" placeholder="Enter Option Two"></Form.Control>
+              </Form.Group>
+              <Button type="submit">Submit</Button>
+            </Form>
           </Card.Body>
         </Card>
       </Container>
